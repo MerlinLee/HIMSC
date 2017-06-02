102999e1a911098563c1737db93be419d81f984f
Merlin Lee <limf5513@mails.jlu.edu.cn>
2017-06-02 15:51:16 +0800

    new_version_20170602_1551

---
+++ app/src/main/java/cn/edu/jlu/limf/himsc/controller/services/UploadServerController/NewBrunchService.java
@@ -0,0 +1,16 @@
+package cn.edu.jlu.limf.himsc.controller.services.UploadServerController;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class NewBrunchService extends Service {
+    public NewBrunchService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
